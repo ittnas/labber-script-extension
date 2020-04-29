286a4520fd9ae3947d35b80c81d1ca22ca4c9bb8
Antti Vepsäläinen <apvepsala@gmail.com>
2020-04-29 19:39:17 -0400

    Fixed bug a in ordering of data with dimensions > 2. Also, works now with log channels having vector data.

--- labber_script_extension/script_log_file.py
+++ labber_script_extension/script_log_file.py
@@ -4,7 +4,6 @@ import os
 sys.path.append(os.environ.get(
     'LABBERPATH', r'C:\Program Files (x86)\Labber\Script'))  # NOQA: E402
 import Labber as Labber
-from Labber import ScriptTools
 
 
 class ScriptLogFile(Labber.LogFile):
@@ -20,21 +19,35 @@ class ScriptLogFile(Labber.LogFile):
             log_channel (str) -- name of the log channel which data is returned.
         """
         step_channels = self.getStepChannels()
+        log_channels = self.getLogChannels()
+        if log_channel is None:
+            log_channel = log_channels[0]['name']
 
+        log_channel_dict = None
+        for log_channel_dict_iter in log_channels:
+            if log_channel_dict_iter['name'] == log_channel:
+                log_channel_dict = log_channel_dict_iter
+                break
         channel_names = []
         channel_values = []
         dimensions = []
         for channel in step_channels:
             if isinstance(channel['values'], np.ndarray) and len(channel['values']) > 1:
-                # print(len(channel['values']))
                 channel_names.append(channel['name'])
                 channel_values.append(channel['values'])
                 dimensions.append(len(channel['values']))
 
-        data = np.transpose(self.getData(log_channel))
-        # print(data.shape)
-        # print(dimensions)
+        data = self.getData(log_channel)
+        if log_channel_dict['vector']:
+            dimensions.insert(0, data.shape[-1])
+            channel_names.insert(0, 'vector_data')
+            channel_values.insert(0, np.linspace(0, 1, data.shape[-1]))
+
+        first_element = dimensions.pop(0)
+        dimensions = dimensions[::-1]
+        dimensions.append(first_element)
+
         data = np.reshape(data, dimensions)
+        data = np.transpose(data)
 
         return (data, channel_values, channel_names)
-        # print(data.shape)
